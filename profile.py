a491e29d67671b3315618b79f01845532fbfb029
HemanthMarella <78777733+HemanthMarella@users.noreply.github.com>
2021-07-21 19:56:56 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,19 +1,34 @@
 import geni.portal as portal
 import geni.rspec.pg as pg
+import geni.rspec.igext as IG
 
 pc = portal.Context()
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.routable_control_ip = "true"
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /local/repository/silly.sh"))
+tourDescription = \
+"""
+This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client who is to be authenticated using LDAP. 
+"""
 
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
 
+for i in range(2):
+  if i == 0:
+    node = request.XenVM("ldapserver")    
+  else:
+    node = request.XenVM("ldapclient")
+   
+  node.routable_control_ip = "true"  
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
 
