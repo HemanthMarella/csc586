65ce1e9f03443a07d8cf1bda5af7a39e2b3bbdcc
HemanthMarella <78777733+HemanthMarella@users.noreply.github.com>
2021-07-26 18:15:29 -0400

    Update profile.py
    sso-nfs

--- profile.py
+++ profile.py
@@ -7,28 +7,31 @@ request = pc.makeRequestRSpec()
 
 tourDescription = \
 """
-This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client who is to be authenticated using LDAP. 
+This profile provides a three-node set to study NFS/SSO. One node will be the LDAP server, one node will be the NFS server, 
+and the remaining node is an NFS client. Both NFS nodes will be authenticated using LDAP. 
 """
-
+#
+# Setup the Tour info with the above description and instructions.
+#  
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-for i in range(2):
+for i in range(3):
   if i == 0:
     node = request.XenVM("ldapserver")    
+  elif i == 1:
+    node = request.XenVM("nfsserver")
   else:
-    node = request.XenVM("ldapclient")
-   
+    node = request.XenVM("nfsclient")
   node.routable_control_ip = "true"  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
+  link.addInterface(iface)  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
 
